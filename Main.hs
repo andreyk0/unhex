9ce7a624259cb3d0fbcb3add91ae6174ee2a8df1
Andrey Kartashov <akartashov@gilt.com>
2016-02-22 11:41:42 -0500

    hex/unhex cli args.

--- Main.hs
+++ Main.hs
@@ -1,28 +1,38 @@
 module Main
   where
 
-import           Data.ByteString.Base16
+import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import           Data.ByteString.Base16
+import qualified Data.ByteString.Builder as BSB
+import qualified Data.ByteString.Lazy as LBS
+import           Data.Foldable
 import           System.Environment
+
 
 main :: IO ()
 main = do
   pName <- getProgName
+  pArgs <- getArgs
 
-  case pName of
-   "unhex" -> unhex
-   "hex"   -> hex
-   _       -> error "expected hex|unhex"
+  let conv = case pName
+               of "unhex" -> unhex
+                  "hex"   -> hex
+                  _       -> error "expected hex|unhex"
 
-unhex :: IO ()
-unhex = do
-  input <- BS.getContents
+  if null pArgs
+  then BS.getContents >>= conv
+  else forM_ pArgs $ \x ->
+         conv $ (LBS.toStrict . BSB.toLazyByteString . BSB.stringUtf8) x
+
+
+unhex :: ByteString -> IO ()
+unhex input = do
   let (out, _) = decode input
   BS.putStr out
 
 
-hex :: IO ()
-hex = do
-  input <- BS.getContents
+hex :: ByteString -> IO ()
+hex input = do
   let out = encode input
   BS.putStr out
